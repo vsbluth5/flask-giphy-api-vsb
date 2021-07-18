7edef287e686d9f7f8e5ec285f5f76e2733e768e
vsbluth5 <vsbluth@gmail.com>
2021-07-18 19:45:00 +0000

    class demo

--- app.py
+++ app.py
@@ -2,15 +2,23 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
 app = Flask(__name__)
 
+app.config['GIPHY_KEY'] = os.getenv("GIPHY_KEY")
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods = ['GET', 'POST'])
+def getgif():
+    user_input = request.form['gifchoice']
+    print(user_input)
+    source = getImageUrlFrom(user_input, app.config['GIPHY_KEY'])
+    return render_template('yourgif.html', pic = source)
--- model.py
+++ model.py
@@ -1,2 +1,16 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+def getImageUrlFrom(query, key):
+    giphy_query=f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
+
+    # get is a method in the requests module
+    response = requests.get(giphy_query).json()
+    # print(response)
+    
+    source = response['data'][2]['images']['fixed_width']['url']
+    print(source)
+    
+    # Until I actually get an image, will return knwon pic
+    # test = "/static/images/micropig.jpg"
+    return source
+    
